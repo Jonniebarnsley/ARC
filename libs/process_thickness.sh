435c41cb2d1fb812c1a22edf5865fc1b7e8bf9c5
J R Barnsley <earjbar@login1.arc3.leeds.ac.uk>
2024-02-18 14:33:54 +0000

    add process script

---
+++ libs/process_thickness.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#$ -cwd -V
+#$ -l h_rt=12:00:00
+#$ -pe smp 10
+#$ -l h_vmem=4G
+#$ -N "process_thickness"
+#$ -m be
+#$ -M earjbar@leeds.ac.uk
+# start time:
+date
+# clear out modules:
+module purge
+
+module load anaconda
+module unload python-libs
+source activate postprocessing
+module load parallel
+NCORES=10
+export PYTHONPATH="${PYTHONPATH}:/home/home01/earjbar/.conda/envs/postprocessing/"
+echo $PYTHONPATH
+
+#parallel -j $NCORES --dry-run echo {} ';' 'python generate_compressed_output_v1.py {} >> ProcLogs/{}_outputs.txt' ::: run*cosmos run*cesm
+parallel -j $NCORES 'python generate_compressed_output_plotsAllSameDir_specVar.py {} thickness >> ProcLogs/{}_outputs.txt' ::: run*control
+
