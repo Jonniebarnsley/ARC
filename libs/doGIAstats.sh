298d27789a141f1a4c0e1b47ad74e937c9bda5fb
Jonnie Barnsley <jonniebarnsley@gmail.com>
2024-02-25 20:00:13 +0000

    add -m mask option

--- libs/doGIAstats.sh
+++ libs/doGIAstats.sh
@@ -12,6 +12,20 @@ EXEC="/nobackup/earjo/gia_stats_exec/gia-stats2d"
 
 doGIAstats() {
     
+    # handle option to include mask with -m
+    local is_mask=false
+    while getopts ":m:" opt; do
+        case $opt in
+            m)  
+                local is_mask=true
+                local MASK="$OPTARG" ;;
+            \?) 
+                echo "Invalid option: -$OPTARG" >&2
+                return 1 ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
     # usage clause    
     if [ "$#" -ne 2 ]; then
         echo "Usage: doGIAstats <plotfiles_path> <destination_path>"
@@ -44,7 +58,11 @@ doGIAstats() {
         if [ ! -e "$GIAstats/$statsfile" ]; then
             count="($current/$totalPlot)"
             echo "$count $filename ..."
-            "$EXEC" "$file" 918.0 1028.0 9.81 0.0 # <p_ice> <p_seawater> <gravity> <sea_level>
+            if $is_mask; then
+                "$EXEC" "$file" 918.0 1028.0 9.81 0.0 "$MASK"
+            else
+                "$EXEC" "$file" 918.0 1028.0 9.81 0.0 # <p_ice> <p_seawater> <gravity> <sea_level>
+            fi
             cat pout.0 >> "$GIAstats/$statsfile" # save output to statsfile
         fi
     done
