822564e228f5550b75d041890e4e8d63a032802b
Jonnie Barnsley <jonniebarnsley@gmail.com>
2024-02-26 23:08:33 +0000

    del

--- libs/doGIAstats.sh
+++ libs/doGIAstats.sh
@@ -6,89 +6,83 @@
 #   <plotfiles_path>    : path to plotfiles directory
 #   <destination_path>  : path to GIAstats directory
 
-source $HOME/libs/count_files.sh
+source $HOME/libs/utils.sh
 
-statstool() {
+usage() { echo "Usage: $0 [-m <mask>] [-b <basin>] <plotfiles_directory> <stats_directory>" 1>&2; exit 1; }
 
-    local EXEC="/nobackup/earjo/gia_stats_exec/gia-stats2d"
+# handle option to include mask and basin
+while getopts ":m:b:" option; do
+    case $option in
+        m)  
+            MASK="$OPTARG"
+            ;;
+        b)
+            BASIN_ID="$OPTARG"
+            ;;
+        *) 
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 
-    # handle option to include mask with -m
-    is_mask=false
-    while getopts ":m:" opt; do
-        case "${opt}" in
-            m)  is_mask=true
-                MASK="${OPTARG}" ;;
-            \?) echo "Invalid option: -${OPTARG}" >&2
-                return 1 ;;
-        esac
-    done
-    shift $((OPTIND - 1))
+case $BASIN_ID in
+    0)  
+        BASIN="WAIS" 
+        ;;
+    1)  
+        BASIN="EAIS" 
+        ;;
+    2)  
+        BASIN="APIS" 
+        ;;
+esac
 
-    local plotfile=$1
+# check args    
+if [ "$#" -ne 2 ]; then
+    usage
+fi
 
-    if [[ $is_mask == true ]]; then
-        echo "loading mask..."
-        #"$EXEC" "$plotfile" 918.0 1028.0 9.81 0.0 "$MASK"
-    else
-        echo "doing stats for whole continent..."
-        #"$EXEC" "$plotfile" 918.0 1028.0 9.81 0.0 # <rho_ice> <rho_seawater> <gravity> <sea_level>
-    fi
-}
+plotfiles="$1"
+GIAstats="$2"
 
-doGIAstats() {
+# make GIAstats dir if it doesn't already exist
+mkdir -p $GIAstats
+
+# if plotfiles and GIAstats are the same size, stop
+totalPlot=$(count_files "$plotfiles" 'plot.*.2d.hdf5')
+totalGIA=$(count_files "$GIAstats" 'plot.*.GIAstats')
+if [ "$totalPlot" -eq "$totalGIA" ]; then
+    exit 1
+fi
+
+# otherwise, iterate over plotfiles
+current=0
+for file in "$plotfiles"/plot.*.2d.hdf5; do
     
-    # handle option to include mask with -m
-    local is_mask=false
-    while getopts ":m:" opt; do
-        case $opt in
-            m)  
-                local is_mask=true
-                local MASK="$OPTARG" ;;
-            \?) 
-                echo "Invalid option: -$OPTARG" >&2
-                return 1 ;;
-        esac
-    done
-    shift $((OPTIND - 1))
+    ((current += 1)) # increment counter
+    count="(${current}/${totalPlot})"
 
-    # usage clause    
-    if [ "$#" -ne 2 ]; then
-        echo "Usage: doGIAstats <plotfiles_path> <destination_path>"
-        return 1
-    fi
+    filename=$(basename "$file")
+    statsfile="${GIAstats}/${filename%.2d.hdf5}.GIAstats"
 
-    local plotfiles="$1"
-    local GIAstats="$2"
-    
-    # make GIAstats dir if it doesn't already exist
-    mkdir -p $GIAstats
+    # if corresponding statsfile doesn't exist, make one
+    if [ ! -e "${statsfile}" ]; then
 
-    # if plotfiles and GIAstats are the same size, stop
-    local totalPlot=$(count_files "$plotfiles" 'plot.*.2d.hdf5')
-    local totalGIA=$(count_files "$GIAstats" 'plot.*.GIAstats')
-    if [ "$totalPlot" -eq "$totalGIA" ]; then
-        return
-    fi
-    
-    # otherwise, iterate over plotfiles
-    local current=0
-    for file in "$plotfiles"/plot.*.2d.hdf5; do
-        
-        ((current += 1)) # increment counter
-        filename=$(basename "$file")
-        statsfile="${filename%.2d.hdf5}.GIAstats"
-        
-        # if corresponding statsfile doesn't exist, make one
-        if [ ! -e "$GIAstats/$statsfile" ]; then
-            count="($current/$totalPlot)"
-            echo "$count $filename ..."
-            if $is_mask; then
-                "$EXEC" "$file" 918.0 1028.0 9.81 0.0 "$MASK"
-            else
-                "$EXEC" "$file" 918.0 1028.0 9.81 0.0 # <rho_ice> <rho_seawater> <gravity> <sea_level>
-            fi
-            cat pout.0 >> "$GIAstats/$statsfile" # save output to statsfile
+        # check for mask
+        if [ -z "${MASK}" ]; then
+            echo "${count} ${filename} ..."
+            bash statstool.sh $file
+            #"$EXEC" "$file" 918.0 1028.0 9.81 0.0
+            
+        else
+            echo "${BASIN} ${count} ${filename} ..."
+            bash statstool.sh -m $MASK -b $BASIN_ID $file
+            #"$EXEC" "$file" 918.0 1028.0 9.81 0.0 "$MASK" "$BASIN_ID"
         fi
-    done
-}
+        
+        cat pout.0 >> "$GIAstats/$statsfile" # save output to statsfile
+    fi
+done
+
 
