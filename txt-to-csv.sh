fb910c02ea7ab31900083aac1c9c0b3118595658
J R Barnsley <earjbar@login2.arc3.leeds.ac.uk>
2024-02-29 22:49:01 +0000

    redone with fix

--- txt-to-csv.sh
+++ txt-to-csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for txt in $CONTROL/run*control/run*/*_summary_stats.txt; do
+for txt in $CONTROL/run*control/run*/APIS_*_control_summary_stats.txt; do
     echo $(basename $txt)
     python $HOME/libs/summary_to_csv.py $txt
 done
