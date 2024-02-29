1d3f502a0c8f045fa5bfce5b5e9226123ee58119
J R Barnsley <earjbar@login2.arc3.leeds.ac.uk>
2024-02-29 02:52:50 +0000

    bugfix

--- templates/job_GIAstats.template.sh
+++ templates/job_GIAstats.template.sh
@@ -4,7 +4,7 @@
 #$ -l h_rt=3:00:00
 #$ -pe smp 1
 #$ -l h_vmem=4G
-#$ -N "@JOBIDstats"
+#$ -N "@JOBID_stats"
 #$ -m be
 #$ -j y
 #$ -M earjbar@leeds.ac.uk
@@ -24,5 +24,5 @@ for BASIN_ID in 0 1 2; do
     BASIN=$(getIMBIEbasin $BASIN_ID)
     basin_stats="GIAstats/${BASIN}"
 
-    bash $HOME/libs/GIAstats.sh -m $MASK -b $BASIN plotfiles $basin_stats
+    bash $HOME/libs/GIAstats.sh -m $MASK -b $BASIN_ID plotfiles $basin_stats
 done
