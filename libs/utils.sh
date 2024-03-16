959213c51d1e49dc8f2011a72c1d0a1da5fe4a49
J R Barnsley <earjbar@login2.arc3.leeds.ac.uk>
2024-03-16 15:49:23 +0000

    add rignot basin

--- libs/utils.sh
+++ libs/utils.sh
@@ -23,5 +23,31 @@ getIMBIEbasin() {
     2)
         echo "APIS"
         ;;
-esac
+    esac
+}
+
+getRignotbasin() {
+    
+    local ID="$1"
+    case $ID in
+    0)      echo "Islands"  ;;
+    1)      echo "H-Hp"     ;;
+    2)      echo "F-G"      ;;
+    3)      echo "E-Ep"     ;;
+    4)      echo "D-Dp"     ;;
+    5)      echo "Cp-D"     ;;
+    6)      echo "B-C"      ;;
+    7)      echo "A-Ap"     ;;
+    8)      echo "Jpp-K"    ;;
+    9)      echo "G-H"      ;;
+    10)     echo "Dp-E"     ;;
+    11)     echo "Ap-B"     ;;
+    12)     echo "C-Cp"     ;;
+    13)     echo "K-A"      ;;
+    14)     echo "J-Jpp"    ;;
+    15)     echo "Ipp-J"    ;;
+    16)     echo "I-Ipp"    ;;
+    17)     echo "Hp-I"     ;;
+    18)     echo "Ep-F"     ;;
+    esac
 }
