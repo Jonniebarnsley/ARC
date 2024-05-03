61ecfe1ad54d70613d94a18f1c87601c46881610
J R Barnsley <earjbar@login2.arc3.leeds.ac.uk>
2024-05-03 15:38:56 +0100

    zwally basins now from 1 to 27

--- templates/job_GIAstats.template.sh
+++ templates/job_GIAstats.template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #$ -cwd -V
-#$ -l h_rt=6:00:00
+#$ -l h_rt=24:00:00
 #$ -pe smp 1
 #$ -l h_vmem=2G
 #$ -N "@JOBID_stats"
@@ -18,11 +18,11 @@ module switch intel gnu
 
 source $HOME/libs/utils.sh
 
-MASK=$HOME/data/ISMIP_basin_mask_16km.hdf5
+MASK=$HOME/data/zwally_basins_extended_16km.hdf5
 
-for BASIN_ID in 0 1 2; do
-    BASIN=$(getIMBIEbasin $BASIN_ID)
-    basin_stats="GIAstats/${BASIN}"
+for BASIN_ID in {1..27}; do
+    BASIN=$(getBasin $MASK $BASIN_ID)
+    basin_stats="GIAstats/Zwally/${BASIN}"
 
     bash $HOME/libs/GIAstats.sh -m $MASK -b $BASIN_ID plotfiles $basin_stats
 done
