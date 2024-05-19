4ddc762a7f0795e3ba977b0f3c6e3f29ec11665c
J R Barnsley <earjbar@login2.arc3.leeds.ac.uk>
2024-05-19 13:37:32 +0100

    moved to libs

--- make_process_jobs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for var in 'calvingFlux' 'calvingRate' 'dragCoef' 'iceFrac' 'sTemp' 'viscosityCoef'; do
-    sed -e s/@VAR/$var/ templates/job.process_var_template.sh > "$CONTROL/process_${var}.sh"
-done
